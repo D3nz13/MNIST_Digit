c368f978adfdbbe823d43d39e98451b50e22d4f0
D3nz13 <seba.szczepan2@gmail.com>
2021-07-08 15:00:15 +0200

    Decreased the number of epochs from 25 to 10

--- mnist.py
+++ mnist.py
@@ -57,5 +57,5 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test = get_data()
     model = create_model()
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    history = model.fit(X_train, y_train, epochs=25, batch_size=128, verbose=0, validation_data=(X_test, y_test))
+    history = model.fit(X_train, y_train, epochs=10, batch_size=128, verbose=1, validation_data=(X_test, y_test))
     make_plots(history)
